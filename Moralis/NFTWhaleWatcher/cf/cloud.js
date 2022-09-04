8e83717af1d624959d053671121e1f52b9760486
Milen Dinev <dinev.milen@gmail.com>
2022-09-04 17:52:23 +0300

    Initial commit

---
+++ Moralis/NFTWhaleWatcher/cf/cloud.js
@@ -0,0 +1,15 @@
+
+Moralis.settings.setAPIRateLimit({
+    anonymous:1000, authenticated:2000, windowMs:60000
+  })
+
+// import {FirebaseFunctionsRateLimiter} from 'firebase-functions-rate-limiter';
+
+// const firestore = admin.firestore(app);
+// const limiter = FirebaseFunctionsRateLimiter.withFirestoreBackend(
+//   {
+//     name: 'rateLimiter',
+//     maxCalls: 100,
+//     periodSeconds: 5,
+//  },
+//  firestore);
